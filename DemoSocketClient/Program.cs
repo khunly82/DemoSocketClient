55d619991812eb18bc49234733e5170592a450bc
Khun <lykhun@gmail.com>
2025-11-06 11:36:22 +0100

    Add project files.

---
+++ DemoSocketClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+// création de la connection au hub
+HubConnection connection = new HubConnectionBuilder()
+    .WithUrl("http://10.10.10.2:39539/ws/message?access_token=Khun")
+    .Build();
+
+// handshake
+await connection.StartAsync();
+
+// ecouter un evenement 
+connection.On<string>("newUser", (user) =>
+{
+    Console.WriteLine($"{user} vient de se connecter");
+});
+
+
+// permet de gerder la console ouverte
+while (true) ;
